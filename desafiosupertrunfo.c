4b341cb1acf1cb9b107e37297ed22e65138d47ba
Pardaldz7 <igordasilvacarvalho.carvalho@gmail.com>
2025-04-01 13:03:46 +0000

    populaçao de sobradinho

---
+++ desafiosupertrunfo.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+int main(){
+    int cidadeSob1 = 1000;
+    int cidadeSob2 = 1500;   
+
+    if(cidadeSob1 > cidadeSob2){
+       printf("A cidadeSob1 tem menor população! \n");
+    } else {
+
+        printf("A cidade que tem o maior numero de populaçao é sobradinho 2");
+    }
+
+    
+
+}
